ec0520f6b3847bf54037b1775d2830aeccd7bc87
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-13 09:31:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Employee from './pages/Employee';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Employee from './pages/Employee';
 import Wrapper from './components/Wrapper';
 import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <Router>
     <div className='App'>
       <Wrapper>
         <Header />
-        <Route exact path="/" component={Employee} />
       </Wrapper>
     </div>
-    </Router>
   );
 }
 
